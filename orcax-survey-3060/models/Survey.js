80f0d86afe70dec0bb8b0b2579e0c89545d688eb
OrcaX <jbl01570@hanmail.net>
2025-05-16 19:48:47 +0900

    Update Survey.js

--- orcax-survey-3060/models/Survey.js
+++ orcax-survey-3060/models/Survey.js
@@ -1,14 +1,13 @@
-// models/Survey.js
 const mongoose = require('mongoose');
 
-const SurveySchema = new mongoose.Schema({
+const surveySchema = new mongoose.Schema({
   name: String,
   satisfaction: String,
-  opinion: String,
+  comment: String,
   date: {
     type: Date,
     default: Date.now
   }
 });
 
-module.exports = mongoose.model('Survey', SurveySchema);
+module.exports = mongoose.model('Survey', surveySchema);
