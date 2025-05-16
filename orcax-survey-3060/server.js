180e8f25d0331af412ee393e9fe54ec39ca0a294
OrcaX <jbl01570@hanmail.net>
2025-05-16 19:49:55 +0900

    Update server.js

--- orcax-survey-3060/server.js
+++ orcax-survey-3060/server.js
@@ -9,14 +9,13 @@ app.use(express.json());
 app.use(express.static('public'));
 
 const port = process.env.PORT || 3060;
+
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('✅ MongoDB 연결 성공'))
   .catch(err => console.error('❌ MongoDB 연결 실패:', err));
 
-const surveyRoutes = require('./routes/survey');
-app.use('/api/survey', surveyRoutes);
+app.use('/api/survey', require('./routes/survey'));
 
 app.listen(port, () => {
   console.log(`🚀 고객 만족도 서버 실행 중 (포트 ${port})`);
 });
-
